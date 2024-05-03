ab3088add08369214add3a2d0aec16e2a34ba92b
Anatoly <u123@ua.fm>
2024-05-03 18:39:52 +0300

    update main urls

--- social_media_api/urls.py
+++ social_media_api/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/user/", include("user.urls", namespace="user")),
     path("api/", include("social_media.urls", namespace="social_media")),
+    path("__debug__/", include("debug_toolbar.urls")),
 ]
