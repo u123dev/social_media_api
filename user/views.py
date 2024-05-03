1fc17574077e869580290ff4c971863b3952bc1e
Anatoly <u123@ua.fm>
2024-05-03 18:26:17 +0300

    add user views & urls

--- user/views.py
+++ user/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import generics, viewsets, status, mixins
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
-# Create your views here.
+from user.permissions import IsOwnerOrReadOnly
+from user.serializers import UserSerializer
+
+
+class CreateUserView(generics.CreateAPIView):
+    serializer_class = UserSerializer
+
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    serializer_class = UserSerializer
+    authentication_classes = (JWTAuthentication,)
+    permission_classes = (IsAuthenticated,)
+
+    def get_object(self):
+        return self.request.user
