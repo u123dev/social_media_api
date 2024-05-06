6fc54302792fb791b7b849d6818d031eda829ded
Anatoly <u123@ua.fm>
2024-05-06 13:00:19 +0300

    create models & admin - Post & Comment

--- social_media/models.py
+++ social_media/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+from social_media_api import settings
+from user.models import User
+
+
+class Post(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(upload_to="upload/post", null=True, blank=True)
+    likes = models.ManyToManyField(User, related_name='likes', blank=True)
+
+    class Meta:
+        ordering = ["-created_at"]
+
+    def __str__(self):
+        return f"post id={self.id} | {self.created_at} | {self.content[:15]} ..."
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="comments")
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["-created_at"]
+
+    def __str__(self):
+        return f"comment id={self.id} | {self.created_at} | {self.message[:15]}"
