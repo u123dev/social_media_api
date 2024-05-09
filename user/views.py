e1b9dceee1697ec0f149cdf6cc760e765d2c5dd5
Anatoly <u123@ua.fm>
2024-05-09 10:55:27 +0300

    add permissions

--- user/views.py
+++ user/views.py
@@ -1,8 +1,7 @@
-from rest_framework import generics, viewsets, status, mixins
+from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
-from user.permissions import IsOwnerOrReadOnly
 from user.serializers import UserSerializer
 
 
