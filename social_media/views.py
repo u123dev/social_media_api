3076af0fbbb9abdaabbfc07928f693448752cfda
Anatoly <u123@ua.fm>
2024-05-09 10:55:07 +0300

    add celery tasks

--- social_media/views.py
+++ social_media/views.py
@@ -1,29 +1,39 @@
+import base64
+from datetime import datetime, timezone
+
 from django.contrib.auth import get_user_model
-from django.core.exceptions import ObjectDoesNotExist
 from django.db.models import Count, Q
 from rest_framework import generics, viewsets, status, mixins
 from rest_framework.decorators import action
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
 
-from social_media.models import Post
-from user.permissions import IsOwnerOrReadOnly
+from social_media.models import Post, Comment
+from social_media.permissions import IsOwnerOrReadOnly, IsOwnerUserOrReadOnly
+from social_media.tasks import publish_post
 from social_media.serializers import (
     UserProfileListSerializer,
     UserProfileDetailSerializer,
     UserProfileSerializer,
     UserProfilePictureSerializer,
-    UserFollowSerializer, PostSerializer, PostDetailSerializer, PostListSerializer, PostUpdateSerializer,
-    PostImageSerializer, CommentSerializer, PostLikeSerializer,
+    UserFollowSerializer,
+    PostSerializer,
+    PostDetailSerializer,
+    PostListSerializer,
+    PostUpdateSerializer,
+    PostImageSerializer,
+    CommentSerializer,
+    PostLikeSerializer,
 )
 
 
 class UserProfileViewSet(
-    #  mixins.CreateModelMixin,
+    # mixins.CreateModelMixin,
     mixins.RetrieveModelMixin,
     mixins.UpdateModelMixin,
-    #  mixins.DestroyModelMixin,
+    # mixins.DestroyModelMixin,
     mixins.ListModelMixin,
     GenericViewSet
 ):
@@ -31,7 +41,7 @@ class UserProfileViewSet(
         followers_count=(Count("followers", distinct=True)),
         followed_by_count=(Count("followed_by", distinct=True)),
     )
-    permission_classes = [IsOwnerOrReadOnly]
+    permission_classes = [IsOwnerUserOrReadOnly]
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
@@ -65,7 +75,7 @@ class UserProfileViewSet(
         methods=["POST", "GET"],
         detail=True,
         url_path="picture",
-        permission_classes=[IsOwnerOrReadOnly],
+        permission_classes=[IsOwnerUserOrReadOnly],
     )
     def set_picture(self, request, pk=None):
         """Endpoint for uploading user profile picture"""
@@ -79,6 +89,7 @@ class UserProfileViewSet(
     @action(detail=True, methods=["POST"],
             permission_classes=[IsAuthenticated])
     def follow(self, request, pk=None):
+        """Add current user to followers"""
         user_to_follow = self.get_object()
         user_to_follow.followers.add(self.request.user)
         return Response(
@@ -90,6 +101,7 @@ class UserProfileViewSet(
     @action(detail=True, methods=["POST"],
             permission_classes=[IsAuthenticated])
     def unfollow(self, request, pk=None):
+        """Delete current user from followers"""
         user_to_unfollow = self.get_object()
         user_to_unfollow.followers.remove(self.request.user)
         return Response(
@@ -98,28 +110,36 @@ class UserProfileViewSet(
             status=status.HTTP_200_OK
         )
 
-    @action(methods=["GET"], detail=True, url_path="followers")
+    @action(methods=["GET"], detail=True, url_path="followers",
+            permission_classes=[IsAuthenticated])
     def get_followers(self, request, pk=None):
+        """List all followers"""
         user = self.get_object()
         serializer = UserFollowSerializer(
             user.followers.all(), many=True
         )
         return Response(serializer.data, status=status.HTTP_200_OK)
 
-    @action(methods=["GET"], detail=True, url_path="followed-by")
+    @action(methods=["GET"], detail=True, url_path="followed-by",
+            permission_classes=[IsAuthenticated])
     def get_followed_by(self, request, pk=None):
+        """List all users that followed by current user """
         user = self.get_object()
         followed_by = user.followed_by.all()
         serializer = UserFollowSerializer(followed_by, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
 
-#########
+class PostPagination(PageNumberPagination):
+    page_size = 3
+    max_page_size = 100
+
+
 class PostViewSet(
     mixins.CreateModelMixin,
     mixins.RetrieveModelMixin,
     mixins.UpdateModelMixin,
-    # mixins.DestroyModelMixin,
+    mixins.DestroyModelMixin,
     mixins.ListModelMixin,
     GenericViewSet
 ):
@@ -128,6 +148,18 @@ class PostViewSet(
         comments_count=Count("comments", distinct=True),
     ).select_related("user")
     serializer_class = PostSerializer
+    pagination_class = PostPagination
+    permission_classes = [
+        IsAuthenticated,
+    ]
+
+    def get_permissions(self):
+        """ Returns the list of permissions that this view requires."""
+        if self.action in ("update", "partial_update"):
+            permission_classes = [IsOwnerOrReadOnly]
+        else:
+            permission_classes = self.permission_classes
+        return [permission() for permission in permission_classes]
 
     def get_serializer_class(self):
         if self.action == "list":
@@ -161,31 +193,34 @@ class PostViewSet(
         return queryset
 
     def create(self, request, *args, **kwargs):
-        user_id = request.user.id
-        # user = request.user
-        user =request.data.get("user")
+        user = request.user
 
         serializer = self.serializer_class(data=request.data)
         serializer.is_valid(raise_exception=True)
         post_at = request.data.get("post_at")
 
         if post_at:
-            try:
-                creator = get_user_model().objects.get(pk=user_id)
-                Post.objects.create(
-                    content=post_at+" *** "+request.data.get("content"),
-                    image=request.FILES.get("image"),
-                    user=creator,
-                )
-            except ObjectDoesNotExist:
-                return Response(
-                    f"Impossible to post by this user: {user_id}",
-                    status=status.HTTP_404_NOT_FOUND
-                )
-            return Response(
-                f"Post will be published at {post_at}",
-                status=status.HTTP_200_OK
+            image = self.request.FILES.get("image")
+            if image:
+                image = image.read()
+                byte = base64.b64encode(image)
+                image_data = {
+                    "image": byte.decode("utf-8"),
+                    "name": self.request.FILES.get("image").name
+                }
+            else:
+                image_data = None
+
+            post_at = (datetime.strptime(post_at, "%Y-%m-%dT%H:%M")
+                       .astimezone(timezone.utc))
+            publish_post.apply_async(
+                args=[request.data.get("content"),
+                      image_data,
+                      request.user.id],
+                eta=post_at
             )
+            return Response(f"Post will be published at {post_at} UTC",
+                            status=status.HTTP_200_OK)
 
         serializer.save(user=user)
         return Response(serializer.data, status=status.HTTP_201_CREATED)
@@ -194,7 +229,7 @@ class PostViewSet(
         methods=["POST", "GET"],
         detail=True,
         url_path="image",
-        # permission_classes=[IsOwnerOrReadOnly],
+        permission_classes=[IsOwnerOrReadOnly],
     )
     def image(self, request, pk=None):
         """Endpoint for uploading post image"""
@@ -207,6 +242,7 @@ class PostViewSet(
 
     @action(methods=["POST"], detail=True)
     def comment(self, request, pk=None):
+        """Endpoint for comment of post"""
         serializer = CommentSerializer(data=request.data)
         serializer.is_valid(raise_exception=False)
         serializer.save(
@@ -218,6 +254,7 @@ class PostViewSet(
     @action(detail=True, methods=["POST"],
             permission_classes=[IsAuthenticated])
     def like(self, request, pk=None):
+        """Endpoint for like of post"""
         post_to_like = self.get_object()
         post_to_like.likes.add(self.request.user)
         return Response(status=status.HTTP_204_NO_CONTENT)
@@ -225,12 +262,31 @@ class PostViewSet(
     @action(detail=True, methods=["POST"],
             permission_classes=[IsAuthenticated])
     def unlike(self, request, pk=None):
+        """Endpoint for inlike of post"""
         post_to_like = self.get_object()
         post_to_like.likes.remove(self.request.user)
         return Response(status=status.HTTP_204_NO_CONTENT)
 
-    @action(detail=False, url_path="liked_posts")
+    @action(detail=False, methods=["GET"], url_path="liked_posts",
+            permission_classes=[IsAuthenticated])
     def liked_posts(self, request, pk=None):
+        """Endpoint for list of all liked post by current user"""
         posts = Post.objects.filter(likes=self.request.user)
         serializer = PostListSerializer(posts, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    permission_classes = [
+        IsOwnerOrReadOnly,
+    ]
+
+    def get_serializer_class(self):
+        if self.action == "retrieve":
+            return CommentSerializer
+        return CommentSerializer
+
+    def get_queryset(self):
+        queryset = self.queryset.filter(user_id=self.request.user.id)
+        return queryset
